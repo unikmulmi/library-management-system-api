e9cd5c4e6a7495ffc473718c396b0bfcb5d8a9e4
unik <unik_mulmi7@hotmail.com>
2026-06-23 22:26:04 +0545

    Model relationships Completed

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Author extends Model
 {
     /** @use HasFactory<\Database\Factories\AuthorFactory> */
     use HasFactory;
+
+    protected $fillable = ['name' , 'bio' , 'nationality'];
+
+    public function books(): HasMany
+    {
+        return $this->hasMany(Book::class);
+    }
 }
