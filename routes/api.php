1b02371757661fadc4ba0c9e4f6368dcc1e7cfa6
unik <unik_mulmi7@hotmail.com>
2026-06-25 14:09:10 +0545

    api.php config for authors , adding hasapitokens to user model

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::apiResource('authors' , AuthorController::class);
